eb8493534a5c5bda7376574fdb27d6fd3cb0d159
Klif <klifb@hotmail.com>
2019-08-04 11:42:22 -0700

    File Update

--- static/js/app.js
+++ static/js/app.js
@@ -6,7 +6,7 @@ function buildMetadata(sample) {
   // Use d3 to select the panel with id of `#sample-metadata`
   var selectMetadata = d3.select('#sample-metadata');
   // Use `.html("") to clear any existing metadata
-  d3.json(`/metadata/${sample}`).then(data => {
+  d3.json(`/samples/${sample}`).then(data => {
     selectMetadata.html("");
     // Use `Object.entries` to add each key and value pair to the panel
     // Hint: Inside the loop, you will need to use d3 to append new
@@ -14,82 +14,53 @@ function buildMetadata(sample) {
     console.log(Object.entries(data));
     Object.entries(data).forEach(([key, value]) => {
       selectMetadata
-        .append('p').text(`${key} : ${value}`)
-        .append('hr')
+      d3.append('p').text(`${key} : ${value}`)
+      d3.append('hr')
     });
   })
-  // function buildMetadata(sample) {
-  //   var url1 = `/samples/${sample}`;
-  //   // @TODO: Complete the following function that builds the metadata panel
-  //   // Use `d3.json` to fetch the metadata for a sample
-  //   d3.json(url1).then(function (data) {
-
-  //     var dataA = [data];
-  //     // Use d3 to select the panel with id of `#sample-metadata`
-  //     var dataM = d3.select("#sample-metadata");
-  //     // Use `.html("") to clear any existing metadata
-  //     dataM.html("");
-  //     // Use `Object.entries` to add each key and value pair to the panel
-  //     // Hint: Inside the loop, you will need to use d3 to append new
-  //     // tags for each key-value in the metadata.
-
-  //     dataP = []
-  //     Object.entries(dataA).forEach(([key, value]) => {
-  //       dataP.push(key, value);
-  //     });
-
-  //     var skup = Object.entries(dataP[1]);
-  //     console.log(skup)
-  //     dataM.selectAll('p').data(skup).enter().append('p')
-  //       .text(function (d) {
-  //         console.log(d[0])
-  //         return `${d[0]}:${d[1]}`
-  //       })
-
-  //   })
-  // }
 
   function buildCharts(sample) {
-    var urls = `/samples/${sample}`;
+
     // @TODO: Use `d3.json` to fetch the sample data for the plots
-    d3.json(urls).then(function (response) {
+    d3.json(selectMetadata).then(function (response) {
       var response = [response];
-      var otuI = response[0].otu_ids;
-      var otuL = response[0].otu_labels;
-      var sampleV = response[0].sample_values;
+      var otuIds = response[0].otu_ids;
+      var otuLabels = response[0].otu_labels;
+      var sampleValues = response[0].sample_values;
+
       // @TODO: Build a Bubble Chart using the sample data
-      var traceb = {
-        x: otuI,
-        y: sampleV,
+      var bubbleTrace = {
+        x: otuIds,
+        y: sampleValues,
         mode: 'markers',
         marker: {
-          size: sampleV,
-          color: otuI
+          size: sampleValues,
+          color: otuIds
         }
       };
 
-      var datab = [traceb];
+      var bubbleData = [bubbleTrace];
 
 
-      Plotly.newPlot("bubble", datab);
+      Plotly.newPlot("bubble", bubbleData);
 
       // // @TODO: Build a Pie Chart
       // // HINT: You will need to use slice() to grab the top 10 sample_values,
       // // otu_ids, and labels (10 each).
 
-      var tracep = {
-        labels: otuI.slice(0, 10),
-        values: sampleV.slice(0, 10),
+      var pieTrace = {
+        labels: otuIds.slice(0, 10),
+        values: sampleValues.slice(0, 10),
         type: 'pie'
       };
 
-      var datap = [tracep];
+      var pieData = [pieTrace];
 
-      var layoutp = {
+      var pieLayout = {
         title: "Pie Chart",
       };
 
-      Plotly.newPlot("pie", datap, layoutp, { showSendToCloud: true });
+      Plotly.newPlot("pie", pieData, pieLayout, { showSendToCloud: true });
     });
   }
 
@@ -122,23 +93,5 @@ function buildMetadata(sample) {
 
   // Initialize the dashboard
   init();
-// function buildMetadata(sample) {
 
-//   // @TODO: Complete the following function that builds the metadata panel
-
-//   // Use `d3.json` to fetch the metadata for a sample
-//   // Use d3 to select the panel with id of `#sample-metadata`
-//   var selectMetadata = d3.select('#sample-metadata');
-//   // Use `.html("") to clear any existing metadata
-//   d3.json(`/metadata/${sample}`).then(data => {
-//     selectMetadata.html("");
-//     // Use `Object.entries` to add each key and value pair to the panel
-//     // Hint: Inside the loop, you will need to use d3 to append new
-//     // tags for each key-value in the metadata.
-//     console.log(Object.entries(data));
-//     Object.entries(data).forEach(([key, value]) => {
-//       selectMetadata
-//         .append('p').text(`${key} : ${value}`)
-//         .append('hr')
-//     });
-//   })
+};
